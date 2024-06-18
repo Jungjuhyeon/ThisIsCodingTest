d9a0a0d75fdc4e366aaab33796017da0b4cff1d5
jungjoo <wjddis000507@gmail.com>
2024-06-19 01:16:31 +0900

    2단원 3.3 1일될때까지 코드 수정후

---
+++ src/_2장_주요_알고리즘/_03_그리디/_3_1이될떄까지_수정코드.java
@@ -0,0 +1,32 @@
+package _2장_주요_알고리즘._03_그리디;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class _3_1이될떄까지_수정코드 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int count =0;
+
+        while (N >=K) {
+            int target = (N/K) *K;
+            count += (N-target);
+            N = target;
+
+            N = N /K ;
+
+            count++;
+        }
+
+        System.out.println(count);
+
+    }
+}
